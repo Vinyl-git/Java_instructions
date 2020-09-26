ef34fa14d6095a4467db24fb91a2d0d7f22e120c
Vinyl-git <contact.marcinblazejewski@gmail.com>
2020-09-26 11:28:11 +0200

    instrukcje

--- src/instructions/Exercise4.java
+++ src/instructions/Exercise4.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 public class Exercise4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Write a number: ");
+        System.out.print("Podaj liczbe: ");
         int num = sc.nextInt();
         String positive = "True";
         String negative = "False";
